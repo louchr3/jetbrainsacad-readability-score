1a53e50d979e36a2f93e66e9ef7f38ef9c910ebc
louchr3 <70855218+louchr3@users.noreply.github.com>
2020-09-24 17:16:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(4752 / (int) Math.pow(10, 1) % 10);
+        System.out.println(4 % 10);
+    }
+}
